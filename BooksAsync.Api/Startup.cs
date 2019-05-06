5f5f0a1d2a4c13b0d42c0429395df6009a962baf
FoxGeoff <fox.geoffrey@gmail.com>
2019-05-06 12:49:02 -0700

    Code Startup class

--- BooksAsync.Api/Startup.cs
+++ BooksAsync.Api/Startup.cs
@@ -1,23 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using BooksAsync.Api.Contexts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 
 namespace BooksAsync.Api
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly IConfiguration _config;
+        private readonly IHostingEnvironment _env;
+        private readonly string _dbconnet;
+
+        public Startup(IConfiguration config, IHostingEnvironment env)
         {
-            Configuration = configuration;
+            _config = config;
+            _env = env;
+            _dbconnet = _config["ConnectionStrings:BooksDbConnectionString"];
         }
 
         public IConfiguration Configuration { get; }
@@ -25,7 +27,24 @@ namespace BooksAsync.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            if (_env.IsDevelopment())
+            {
+                // register the DbContext on the container, getting the connection string from
+                // appSettings (note: use this only for development: in a production enviroment,
+                // use an eviroment variable OR a 'user secret')
+                //TODO:
+                /* options.UseMySql(_config.GetConnectionString("dbconnect"))); */
+
+                services.AddDbContext<BookContext>(option =>
+                    option.UseSqlServer(_dbconnet));
+
+                //TODO: 
+                /* services.AddScoped<ICustomersRepository, CustomersRepository>(); */
+
+            }
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                 .AddJsonOptions(opt => opt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +53,16 @@ namespace BooksAsync.Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
+                //TODO:  
+                /* AudioImageSeeder.InitializeData(app.ApplicationServices, loggerFactory); */
+
+                app.UseCors(builder =>
+                    builder
+                        .AllowAnyOrigin()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                    );
             }
             else
             {
