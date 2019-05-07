a6a93d34dba6e94a3edcd9a358cbe26e04a066ae
FoxGeoff <fox.geoffrey@gmail.com>
2019-05-07 13:03:13 -0700

    Add BooksController

---
+++ BooksAsync.Api/Controllers/BooksController.cs
@@ -0,0 +1,29 @@
+﻿using BooksAsync.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksAsync.Api.Controllers
+{
+    [Route("api/books")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBooksRepository _booksRepository;
+
+        public BooksController(IBooksRepository booksRepository)
+        {
+            _booksRepository = booksRepository
+                ?? throw new ArgumentNullException(nameof(booksRepository));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBooksAsync()
+        {
+            var bookEntities = await _booksRepository.GetBooksAsync();
+            return Ok(bookEntities);
+        }
+    }
+}
