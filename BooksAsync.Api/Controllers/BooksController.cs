cc504461e270d8c90e1e12b93f4bd82f50d4b22a
FoxGeoff <fox.geoffrey@gmail.com>
2019-05-13 17:23:57 -0700

    Add Model BokForCreation

--- BooksAsync.Api/Controllers/BooksController.cs
+++ BooksAsync.Api/Controllers/BooksController.cs
@@ -26,8 +26,9 @@ namespace BooksAsync.Api.Controllers
             return Ok(bookEntities);
         }
 
-        [HttpGet("{id}")]
+        [HttpGet]
         [BookResultFilter]
+        [Route("{id}")]
         public async Task<IActionResult> GetBook(Guid id)
         {
             var bookEntity = await _booksRepository.GetbookAsync(id);
