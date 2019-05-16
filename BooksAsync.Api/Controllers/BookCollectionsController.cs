1186b4c873cb3304b3720fa201afa72dd1170f02
FoxGeoff <fox.geoffrey@gmail.com>
2019-05-16 12:24:49 -0700

    Add Controller BookCollections stub

---
+++ BooksAsync.Api/Controllers/BookCollectionsController.cs
@@ -0,0 +1,28 @@
+﻿using BooksAsync.Api.Models;
+using BooksAsync.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksAsync.Api.Controllers
+{
+    public class BookCollectionsController : ControllerBase
+    {
+        private readonly IBooksRepository _booksRepository;
+
+        public BookCollectionsController(IBooksRepository booksRepository)
+        {
+            _booksRepository = booksRepository ??
+                throw new ArgumentNullException(nameof(booksRepository));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBooksCollection([FromBody] IEnumerable<BookForCreation> books)
+        {
+
+            return Ok();
+        }
+    }
+}
