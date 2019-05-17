01c2d0b4eb39a93245e516f9eac9afbc1cc239ea
FoxGeoff <fox.geoffrey@gmail.com>
2019-05-16 17:01:36 -0700

    Add Bulk Insrt Books

--- BooksAsync.Api/Controllers/BookCollectionsController.cs
+++ BooksAsync.Api/Controllers/BookCollectionsController.cs
@@ -1,26 +1,49 @@
-﻿using BooksAsync.Api.Models;
+﻿using AutoMapper;
+using BooksAsync.Api.Models;
 using BooksAsync.Api.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace BooksAsync.Api.Controllers
 {
+    [Route("api/bookscollection")]
+    [ApiController]
     public class BookCollectionsController : ControllerBase
     {
         private readonly IBooksRepository _booksRepository;
+        private readonly IMapper _mapper;
 
-        public BookCollectionsController(IBooksRepository booksRepository)
+        public BookCollectionsController(IBooksRepository booksRepository, IMapper mapper)
         {
             _booksRepository = booksRepository ??
                 throw new ArgumentNullException(nameof(booksRepository));
+            _mapper = mapper ??
+                throw new ArgumentNullException(nameof(mapper));
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreateBooksCollection([FromBody] IEnumerable<BookForCreation> bookCollection)
+        public async Task<IActionResult> CreateBookCollection([FromBody] IEnumerable<BookForCreation> bookCollection)
         {
+            var bookEntities = _mapper.Map<IEnumerable<Entities.Book>>(bookCollection);
+
+            foreach (var bookEntity in bookEntities)
+            {
+                _booksRepository.AddBook(bookEntity);
+            }
+
+            await _booksRepository.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        //api/bookcollection/(id1,id2)
+        [HttpGet("({bookIds})")]
+        public async Task<IActionResult> GetBookCollection([ModelBinder(BinderType = typeof(ArrayModelBinder<Guid>))] IEnumerable<Guid> bookIds)
+        {
+            //var bookEntities = await _booksRepository.GetbooksAsync(bookIds);
 
             return Ok();
         }
